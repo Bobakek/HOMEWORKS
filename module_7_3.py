57547ebcdcd62c3219b0b5b3509182304049ba57
Bobakek <165632522+Bobakek@users.noreply.github.com>
2024-08-02 13:09:41 +0400

    Update module_7_3.py

--- module_7_3.py
+++ module_7_3.py
@@ -1,15 +1,28 @@
+import os
+import string
+
+
 class WordsFinder:
     def __init__(self, *file_names):
         self.file_names = list(file_names)
+        self._ensure_files_exist()
+
+    def _ensure_files_exist(self):
+        for file_name in self.file_names:
+            if not os.path.exists(file_name):
+                with open(file_name, 'w', encoding='utf-8') as file:
+                    pass  # Создание пустого файла, если он не существует
+
+    def _clean_word(self, word):
+        # Удаление пунктуации и перевод слова в нижний регистр
+        return word.strip(string.punctuation).lower()
 
     def get_all_words(self):
         all_words = {}
         for file_name in self.file_names:
-            
             with open(file_name, 'r', encoding='utf-8') as file:
-                words = file.read().split()  # Разбивка на слова
+                words = [self._clean_word(word) for word in file.read().split()]
                 all_words[file_name] = words
-
         return all_words
 
     def find(self, search_word):
@@ -33,9 +46,3 @@ finder2 = WordsFinder('test_file.txt')
 print(finder2.get_all_words())  # Все слова
 print(finder2.find('TEXT'))  # Позиции слова "TEXT"
 print(finder2.count('teXT'))  # Количество слова "teXT" в файле
-
-
-
-
-
-
