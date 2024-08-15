48aeb92c73d991199d15adc7f258263cc250bc42
Bobakek <165632522+Bobakek@users.noreply.github.com>
2024-08-15 14:39:22 +0400

    Update module_9_1.py

--- module_9_1.py
+++ module_9_1.py
@@ -1,9 +1,19 @@
-def apply_all_func(int_list, *functions):
-    result = [] # list to store the result
-    for i in functions:
-        result.append((i.__name__, i(int_list)))
+# def apply_all_func(int_list, *functions):
+#     result = [] # list to store the result
+#     for i in functions:
+#         result.append((i.__name__, i(int_list)))
 
+#     return result
+
+# print(apply_all_func([6, 20, 15, 9], max, min))
+# print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
+
+def apply_all_func(int_list, *functions):
+    result = {}  # Используем словарь для хранения результатов
+    for func in functions:
+        result[func.__name__] = func(int_list)
     return result
 
+# Примеры вызова функции
 print(apply_all_func([6, 20, 15, 9], max, min))
 print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
