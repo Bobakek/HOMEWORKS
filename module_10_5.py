8c0b0fc281f345fbc9ac5b194681bb972cb4de9d
Bobakek <165632522+Bobakek@users.noreply.github.com>
2024-09-18 13:56:23 +0400

    Update module_10_5.py

--- module_10_5.py
+++ module_10_5.py
@@ -10,7 +10,8 @@ def read_info(name):
             line = f.readline()
             while line:
                 line = f.readline()
-                all_data.append(line.strip())
+                # all_data.append(line.strip())
+                all_data.append(line[0:-1])
                 line = f.readline()
     except UnicodeDecodeError:
         try:
@@ -26,18 +27,20 @@ def read_info(name):
 
 
 if __name__ == "__main__":
-    filenames = [f'./file {number}.txt' for number in range(1, 4)] # Список файлов
-    start = time.time()
-    with Pool(processes=4) as pool:
-        pool.map(read_info, filenames)  # Запускаем функцию для каждого файла (3.1 секунды)
-    finish = time.time()
-    print(f"Время выполнения: {finish - start} секунд")
+    # filenames = [f'./file {number}.txt' for number in range(1, 5)] # Список файлов
+    # start = time.time()
+    # with Pool(processes=4) as pool:
+    #     pool.map(read_info, filenames)  # Запускаем функцию для каждого файла (3.1 секунды)
+    # finish = time.time()
+    # print(f"Время выполнения: {finish - start} секунд")
 
-    filenames = [f'./file {number}.txt' for number in range(1, 4)] # Список файлов
+    filenames = [f'./file {number}.txt' for number in range(1, 5)] # Список файлов
     start = time.time()
     for file in filenames:
         read_info(file)
     finish = time.time()
     print(f"Время выполнения: {finish - start} секунд")
+
+
 
 
