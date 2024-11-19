1ab9539aa78698f5250750c6164caa130d6d81a1
Bobakek <165632522+Bobakek@users.noreply.github.com>
2024-11-19 09:01:49 +0400

    Update module_16_4.py

--- module_16_4.py
+++ module_16_4.py
@@ -1,22 +1,24 @@
-from fastapi import FastAPI, status, Body, HTTPException
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List
+
 app = FastAPI()
-
-
-users = []
-
 class User(BaseModel):
     id: int
     username: str
     age: int
 
-@app.get("/users")
-async def get_users() -> list:
-    """Возвращает всех пользователей."""
+# Список пользователей
+users: List[User] = []
+
+# Модель пользователя
+
+@app.get("/users", response_model=List[User])
+async def get_users() -> List[User]:
+    """Возвращает список всех пользователей."""
     return users
 
-@app.post("/user/{username}/{age}")
+@app.post("/user/{username}/{age}", response_model=User)
 async def add_user(username: str, age: int) -> User:
     """Добавляет нового пользователя."""
     new_id = users[-1].id + 1 if users else 1
@@ -24,8 +26,7 @@ async def add_user(username: str, age: int) -> User:
     users.append(new_user)
     return new_user
 
-@app.put("/user/{user_id}/{username}/{age}")
-
+@app.put("/user/{user_id}/{username}/{age}", response_model=User)
 async def update_user(user_id: int, username: str, age: int) -> User:
     """Обновляет данные пользователя."""
     for user in users:
@@ -35,7 +36,7 @@ async def update_user(user_id: int, username: str, age: int) -> User:
             return user
     raise HTTPException(status_code=404, detail="User was not found")
 
-@app.delete("/user/{user_id}")
+@app.delete("/user/{user_id}", response_model=User)
 async def delete_user(user_id: int) -> User:
     """Удаляет пользователя из списка."""
     for user in users:
