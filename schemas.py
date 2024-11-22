717bee8572c15063b88a583555a3c674bbd4735a
Bobakek <165632522+Bobakek@users.noreply.github.com>
2024-11-22 11:07:29 +0400

    Add files via upload

---
+++ schemas.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel
+
+# Схемы для пользователей
+class CreateUser(BaseModel):
+    username: str
+    firstname: str
+    lastname: str
+    age: int
+
+class UpdateUser(BaseModel):
+    firstname: str
+    lastname: str
+    age: int
+
+# Схемы для задач
+class CreateTask(BaseModel):
+    title: str
+    content: str
+    priority: int
+
+class UpdateTask(CreateTask):
+    pass
