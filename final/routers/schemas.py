8b29104760eb39ef579d481c142a3e08673be2fe
Bobakek <165632522+Bobakek@users.noreply.github.com>
2024-11-27 11:26:01 +0400

    Add files via upload

---
+++ final/routers/schemas.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+
+# Модель для создания пользователя
+class CreateUser(BaseModel):
+    username: str
+    firstname: str
+    lastname: str
+    age: int
+
+# Модель для обновления пользователя
+class UpdateUser(BaseModel):
+    firstname: str
+    lastname: str
+    age: int
